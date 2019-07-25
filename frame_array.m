8357d2ec61c234bdd21504a2f39c96c92e630a7f
athenahughes <53144104+athenahughes@users.noreply.github.com>
2019-07-25 17:18:35 +0200

    Add files via upload

---
+++ frame_array.m
@@ -0,0 +1,78 @@
+for activity = 1:18
+    
+    if activity == 1
+        
+        columns = 30;
+        
+    elseif activity == 2
+            
+        columns = 30;
+        
+    elseif activity == 3
+        
+        columns = 30;
+        
+    elseif activity == 4
+        
+        columns = 30;
+    
+    elseif activity == 5
+        
+        columns = 90;
+        
+    elseif activity == 6
+        
+        columns = 60;
+    
+    elseif activity == 7
+        
+        columns = 60;
+        
+    elseif activity == 8 
+        
+        columns = 90;
+        
+    elseif activity == 9
+        
+        columns = 90;
+        
+    elseif activity == 10
+        
+        columns = 60;
+        
+    elseif activity == 11
+    
+        columns = 180;
+    
+    elseif activity == 12
+    
+        columns = 90;
+        
+    elseif activity == 13
+     
+        columns = 60;
+    
+    elseif activity == 14
+    
+        columns = 90;
+        
+    elseif activity == 15
+    
+        columns = 90;
+    
+    elseif activity == 16
+    
+        columns = 90;
+        
+    elseif activity == 17
+    
+        columns = 90;
+        
+    else
+        
+        if activity == 18
+            
+            columns = 90;
+            
+        end
+        
---
+++ iteration_through_datafiles.m
@@ -0,0 +1,81 @@
+for activity = 1:18
+    
+    if activity == 1
+        
+        repetition_lim = 5;
+        
+    elseif activity == 2
+            
+        repetition_lim = 5;
+        
+    elseif activity == 3
+        
+        repetition_lim = 5;
+        
+    elseif activity == 4
+        
+        repetition_lim = 5;
+    
+    elseif activity == 5
+        
+        repetition_lim = 3;
+        
+    elseif activity == 6
+        
+        repetition_lim = 1;
+    
+    elseif activity == 7
+        
+        repetition_lim = 4;
+        
+    elseif activity == 8 
+        
+        repetition_lim = 3;
+        
+    elseif activity == 9
+        
+        repetition_lim = 3;
+        
+    elseif activity == 10
+        
+        repetition_lim = 3;
+        
+    elseif activity == 11
+    
+        repetition_lim = 3;
+    
+    elseif activity == 12
+    
+        repetition_lim = 3;
+        
+    elseif activity == 13
+     
+        repetition_lim = 3;
+    
+    elseif activity == 14
+    
+        repetition_lim = 3;
+        
+    elseif activity == 15
+    
+        repetition_lim = 3;
+    
+    elseif activity == 16
+    
+        repetition_lim = 3;
+        
+    elseif activity == 17
+    
+        repetition_lim = 3;
+        
+    else
+        
+        if activity == 18
+            
+            repetition_lim = 3;
+            
+        end
+        
+   
+        
+        
